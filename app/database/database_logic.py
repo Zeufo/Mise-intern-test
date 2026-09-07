092c463fd016db0cd58362f60ee542d296bf7531
Zeufo <p.yudin.work@gmail.com>
2026-09-07 19:19:59 +0400

    feat(database): add tables model

--- app/database/database_logic.py
+++ app/database/database_logic.py
@@ -1,8 +1,12 @@
 import abc
 import typing
 
-from config import DATABASE_URL
+from core.config import DATABASE_URL
+from loguru import logger
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+from database.models import Base, Booking
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncLocalSession = async_sessionmaker(engine)
@@ -26,14 +30,29 @@ class AsyncDataBaseCRUD(abc.ABC):
         pass
 
 
+def DataBaseInit() -> None:
+    try:
+        logger.info("Creating tables...")
+        Base.metadata.create_all(engine.sync_engine)
+        logger.info("Tables created")
+
+    except Exception as e:
+        logger.critical("Cant create tables", e)
+
+
+@typing.final
 class AsyncBookingCRUD(AsyncDataBaseCRUD):
     @staticmethod
     async def add_booking(session: AsyncSession) -> typing.Any:
-        pass
+        query = insert(Booking).values()
+        await session.execute(query)
+        await session.commit()
 
     @staticmethod
     async def get_all_bookings(session: AsyncSession) -> typing.Any:
-        pass
+        query = select(Booking)
+        result = await session.execute(query)
+        return result.scalars().all()
 
     @staticmethod
     async def get_exact_booking(session: AsyncSession) -> typing.Any:
--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
+import asyncio
 
+from loguru import logger
+from process import Process
+
+
+async def main() -> None:
+    Process.prepare()
+    await Process.run()
+
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    logger.critical("keyboard interrupt!")
